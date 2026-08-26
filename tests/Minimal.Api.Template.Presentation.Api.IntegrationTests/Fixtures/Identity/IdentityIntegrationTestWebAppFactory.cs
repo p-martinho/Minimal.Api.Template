ccb800213ec0cedd4540d3e3eab99bdc10ff1edd
p-martinho <martinho.pj@gmail.com>
2026-08-26 08:41:27 +0100

    Define the image for SQL test containers as the latest (mcr.microsoft.com/mssql/server:2022-latest)

--- tests/Minimal.Api.Template.Presentation.Api.IntegrationTests/Fixtures/Identity/IdentityIntegrationTestWebAppFactory.cs
+++ tests/Minimal.Api.Template.Presentation.Api.IntegrationTests/Fixtures/Identity/IdentityIntegrationTestWebAppFactory.cs
@@ -11,7 +11,7 @@ namespace Minimal.Api.Template.Presentation.Api.IntegrationTests.Fixtures.Identi
 
 public sealed class IdentityIntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    private const string MsSqlImageName = "mcr.microsoft.com/mssql/server:2022-CU24-ubuntu-22.04";
+    private const string MsSqlImageName = "mcr.microsoft.com/mssql/server:2022-latest";
 
     private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder(MsSqlImageName).Build();
 
